7051f49856cee0bae1bfc2263f380ed5924a041b
trymny <trymny@gmail.com>
2022-03-16 20:43:53 +0100

    Added a gradient correlation method for sub-pixel estimation, cleaned up code

--- func.py
+++ func.py
@@ -5,7 +5,7 @@
  Info:
 
  Written by: Trym Nygård 
- Last updated: Februar 2022
+ Last updated: March 2022
  
 '''
 
@@ -13,8 +13,8 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 import cv2 as cv
-from scipy import signal, misc
 from mpl_toolkits.mplot3d import Axes3D
+
 
 def stereoImgResize(imgL,imgR, scale_percent,interMethod):
     width = int(imgL.shape[1] * scale_percent / 100)
@@ -33,10 +33,6 @@ def stereoImgCrop(img,X,Y,N,M):
     #print("Window size: ", winL.shape)
     return win
 
-def stereoImgAddNoise(img):
-    img = (img - np.mean(img)) / np.std(img)
-    return img 
-
 def windowFunc(funcType, win):
     width,height = win.shape
     Ww=eval('np.'+funcType+'(width)')
@@ -50,7 +46,7 @@ def magMask(dft,param=0):
         maxValue = abs(np.max(dft[i]))
         if(maxValue <= param):
             dft[i] = 0.00001   # zeroes out row i
-            dft[:,i] = 0.00001 # zeroes out column i
+            #dft[:,i] = 0.00001 # zeroes out column i
     return dft
 
 def radMask(dft,param=0.6):
@@ -59,7 +55,7 @@ def radMask(dft,param=0.6):
     maskedDft = dft[int(dft.shape[0]/2-radius):int(dft.shape[0]/2+radius),int(dft.shape[0]/2-radius):int(dft.shape[0]/2+radius)]
     return maskedDft
 
-def computeSubPixelShift(dftL,dftR, radius=0.6, magThres=0):
+def computeSubSpaceID(dftL,dftR, radius=0.6, magThres=0):
     
     M = dftL.shape[0]
     N = dftL.shape[1]
@@ -73,7 +69,7 @@ def computeSubPixelShift(dftL,dftR, radius=0.6, magThres=0):
     
     #Mask out spectral components for DFT's that have peaks with magnitudes less than a threshold value
     #maskedDftL = magMask(maskedDftL,magThres)
-    maskedDftR = magMask(maskedDftR,magThres)
+    #maskedDftR = magMask(maskedDftR,magThres)
 
     # Normalized cross-power spectrum 
     R = (maskedDftL*np.conjugate(maskedDftR))/(np.abs(maskedDftL*np.conjugate(maskedDftR)))  
@@ -93,9 +89,10 @@ def computeSubPixelShift(dftL,dftR, radius=0.6, magThres=0):
 
     angleV = np.angle(dominantV)  #Find the angle
     unwrapV = np.unwrap(angleV)
+
     fittedLineV, res,rank,s = np.linalg.lstsq(A,unwrapV,rcond=-1) #Equation 6 inv(R^TR)R^Tunwrap(v)
     muX = fittedLineV[0] #slope of the fitted line
-    #cX = fittedLineV[1] #abscissa of the fitted line
+    cX = fittedLineV[1] #abscissa of the fitted line
     subShiftX = muX * (M / (2*np.pi)) #translational shift
 
     angleU = np.angle(dominantU)  #Find the angle
@@ -105,14 +102,86 @@ def computeSubPixelShift(dftL,dftR, radius=0.6, magThres=0):
     #cY = fittedLineU[1] #abscissa of the fitted line
     subShiftY = muY * (N / (2*np.pi)) #translational shift
 
+    ''' 
+    temp = unwrapV.shape[0]
+    fx= (np.linspace(-temp/2,temp/2-1,temp)/temp);    # FM = number of pixel in x direction 
+    fx = fx *2 * np.pi                    # Multiply with 2pi to get the (spatial) frequency 
+    fx = np.reshape(fx, (-1, 1))          # Go from shape (M,) to (M,1)
+
+    fig, axarr = plt.subplots(nrows=1, ncols=2, figsize=(5, 3))
+    axarr[0].set_title("Phase difference")
+    axarr[0].grid(True)
+    axarr[0].plot(fx, angleV)
+    axarr[1].set_title("Unwrapped Phase difference")
+    axarr[1].grid(True)
+    
+    x = np.linspace(0,temp,temp)
+    axarr[1].plot(x, unwrapV)
+    axarr[1].plot(x, muX*x+cX)
+    plt.show()
+    '''
+    
     return subShiftX,subShiftY
 
-def computePOC(dftL,dftR):
+def computeGradCorr(img, img2, polyDeg = 2, nPoints = 200, plot = False):
+    M = img.shape[0]
+
+    gHor = np.zeros_like(img)
+    gVer = np.zeros_like(img)
+    gHor2 = np.zeros_like(img2)
+    gVer2 = np.zeros_like(img2)
+
+    for i in range(1,img.shape[0]-1):
+        for j in range(1,img.shape[1]-1):
+            gVer[i,j] = (int(img[i+1,j])-int(img[i-1,j]))
+            gHor[i,j] = (int(img[i,j+1])-int(img[i,j-1]))
+            gVer2[i,j] = (int(img2[i+1,j])-int(img2[i-1,j]))
+            gHor2[i,j] = (int(img2[i,j+1])-int(img2[i,j-1]))
+    
+    g = gHor - np.imag(gVer)
+    g2 = gHor2 - np.imag(gVer2)
+
+    dftG = (np.fft.fft2(g))
+    dftG2 = (np.fft.fft2(g2))
+    
+    cc = np.fft.ifft2(dftG*np.conjugate(dftG2))
+     
+    for i in range(int(M/2)):
+        cc = np.roll(cc, -1, axis=1)
+
+    #fc.plotSurface(cc)
+    
+    idxMax = np.unravel_index(cc.argmax(), cc.shape) 
+    #print(idxMax)
+    
+    dist = 1
+    y = np.array([cc[idxMax[0],idxMax[1]-dist], cc[idxMax[0],idxMax[1]], cc[idxMax[0],idxMax[1]+dist]])
+    x = np.linspace(idxMax[1]-dist,idxMax[1]+dist,3).astype(int)
+
+    fit = np.polyfit(x,y,polyDeg)
+    p = np.poly1d(fit) 
+    newX = np.linspace(idxMax[1]-dist,idxMax[1]+dist,nPoints)
+    
+    if plot:
+        temp = M
+        xx = np.linspace(0,temp,temp).astype(int)  
+        fig, axarr = plt.subplots(1,1)
+        axarr.grid(True)
+        axarr.plot(xx,cc[idxMax[0],:] )
+        axarr.plot(newX,p(newX))
+        axarr.plot(x[0],y[0], marker="o", color="green")
+        axarr.plot(x[1],y[1], marker="o", color="green")
+        axarr.plot(x[2],y[2], marker="o", color="green")
+        plt.show()
+    
+    return int(M/2)-newX[np.argmax(p(newX))]
+
+def computePOC(dftL,dftR, plot = False):
 
     # Normalized cross-power spectrum 
     R = (dftL*np.conjugate(dftR))/(np.abs(dftL*np.conjugate(dftR)))  
 
-    # Taking the inverse discrete Fourier transform to find the phase correlation (Equation 7)
+    # Taking the inverse discrete Fourier transform to find the phase correlation
     POC = np.fft.ifft2(R)
 
     # Using the fourier shif theorem to move the maximum peak
@@ -124,18 +193,31 @@ def computePOC(dftL,dftR):
     # Using a 2d Gaussian distribution to smooth the phase correlation and to remove false peaks
     POC = cv.GaussianBlur(POC,(5,5),0)
 
+    if plot:
+        print("POC Max Peak: ", np.max(POC))
+        plt.figure()
+        plt.imshow(POC,cmap='gray')
+        plt.show()
+
     return POC
 
-def similarityMeasure(imgL, imgR,X_L, Y_L, M, N, method="TM_CCORR_NORMED", winFunc = "blackman"):
+def computePOCshift(POC):
+    # Determine the location of the max peak in the POC function
+    idxPeak = np.unravel_index(POC.argmax(), POC.shape) # Extract indices of max peak from POC function 
+    intShiftY = (POC.shape[0]/2-idxPeak[0])       # Compute integer shift Y from the max peak in POC function
+    intShiftX = (POC.shape[1]/2-idxPeak[1])       # Compute integer shift X from the max peak in POC function
+    return intShiftX,intShiftY
+
+def blockMatching(imgL, imgR,X_L, Y_L, M, N, method="TM_CCORR_NORMED", winFunc = "blackman"):
 
     # Creating a window around the feature
     winL = stereoImgCrop(imgL, X_L, Y_L, M, N)
 
     # Applying blackman window 
-    winL = windowFunc("blackman",winL) #funcType: blackman, hanning,
+    winL = windowFunc(winFunc,winL) #funcType: blackman, hanning,
 
-    x = X_L-200
-    maxPeak = []
+    x = X_L-int(imgL.shape[0]/4)
+    maxVal = []
     xList = []
     while(x < X_L+M):
         winR = stereoImgCrop(imgR, x, Y_L, M, N) 
@@ -144,31 +226,38 @@ def similarityMeasure(imgL, imgR,X_L, Y_L, M, N, method="TM_CCORR_NORMED", winFu
         if method == "TM_CCORR_NORMED":
             res = cv.matchTemplate(winR.astype(np.float32),winL.astype(np.float32),cv.TM_CCORR_NORMED)
             min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-            maxPeak.append(max_val)
+            maxVal.append(max_val)
         elif method == "TM_CCOEFF_NORMED":
             res = cv.matchTemplate(winR.astype(np.float32),winL.astype(np.float32),cv.TM_CCOEFF_NORMED)
             min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-            maxPeak.append(max_val)
+            maxVal.append(max_val)
         elif method == "POC":
             dftL = np.fft.fft2(winL)
             dftR = np.fft.fft2(winR)
             POC = computePOC(dftL,dftR)
-            maxPeak.append(np.max(POC))
+            maxVal.append(np.max(POC))
         
         xList.append(x)
         x=x+1
-    #print("maxPeak: ", maxPeak[np.argmax(maxPeak)], " x: ", xList[np.argmax(maxPeak)])
-    winR = stereoImgCrop(imgR, xList[np.argmax(maxPeak)], Y_L, M, N) 
+    print("maxVal: ", maxVal[np.argmax(maxVal)], " x: ", xList[np.argmax(maxVal)])
+    winR = stereoImgCrop(imgR, xList[np.argmax(maxVal)], Y_L, M, N) 
     winR = windowFunc(winFunc,winR)
-    return winL, winR,xList[np.argmax(maxPeak)]
+    return winL, winR,xList[np.argmax(maxVal)]
 
-def plotPhaseDifference(dftL,dftR,rStepSize=2, cStepSize=2, aa=True):
+def plotPhaseDifference(dftL,dftR,radius=0.6,rStepSize=2, cStepSize=2, aa=True):
 
     dftShiftL = np.fft.fftshift(dftL)
     dftShiftR = np.fft.fftshift(dftR)
 
-    # Normalized cross-power spectrum 
-    R = (dftShiftL*np.conjugate(dftShiftR))/(np.abs(dftShiftL*np.conjugate(dftShiftR)))  
+    #Mask out spectral components that lie outside a radius from the central peak
+    maskedDftL = radMask(dftShiftL,radius)
+    maskedDftR = radMask(dftShiftR,radius)
+    
+    #Mask out spectral components for DFT's that have peaks with magnitudes less than a threshold value
+    #maskedDftL = magMask(maskedDftL,magThres)
+    #maskedDftR = magMask(maskedDftR,0)
+
+    R = (maskedDftL*np.conjugate(maskedDftR))/(np.abs(maskedDftL*np.conjugate(maskedDftR)))  
     
     theta = np.arctan2(R.imag,R.real)  #Can also use theta = np.angle(R) 
 
@@ -180,40 +269,25 @@ def plotPhaseDifference(dftL,dftR,rStepSize=2, cStepSize=2, aa=True):
     ax.plot_wireframe(xData,yData,theta,rstride=rStepSize, cstride=cStepSize,antialiased=aa)
     plt.show()
 
-def computePOCshift(POC):
-    # Determine the location of the max peak in the POC function
-    idxPeak = np.unravel_index(POC.argmax(), POC.shape) # Extract indices of max peak from POC function
-    intShiftY = (POC.shape[0]/2-idxPeak[0])       # Compute integer shift Y from the max peak in POC function
-    intShiftX = (POC.shape[1]/2-idxPeak[1])       # Compute integer shift X from the max peak in POC function
-    return intShiftX,intShiftY
-
-def plotPOCsurface(POC, wireframe=False,rStepSize=1, cStepSize=1, aa=True):
+def plotSurface(func, wireframe=False,rStepSize=1, cStepSize=1, aa=True):
     #*************************Phase correlation surface plot*********************************
-    xx, yy = np.mgrid[(-POC.shape[0]/2):(POC.shape[0]/2), (-(POC.shape[1])/2):(POC.shape[1]/2)]
+    xx, yy = np.mgrid[(-func.shape[0]/2):(func.shape[0]/2), (-(func.shape[1])/2):(func.shape[1]/2)]
 
     #Add fancy color mapping
-    minn, maxx = POC.min(), POC.max()
+    minn, maxx = func.min(), func.max()
     norm = matplotlib.colors.Normalize(minn, maxx)
     m = plt.cm.ScalarMappable(norm=norm, cmap='jet')
     m.set_array([])
-    fcolors = m.to_rgba(POC)
+    fcolors = m.to_rgba(func)
 
     #create the figure
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     if not wireframe:
-        ax.plot_surface(xx, yy, POC , facecolors=fcolors,rstride=rStepSize, cstride=cStepSize,linewidth=0, antialiased=aa, shade=False)
+        ax.plot_surface(xx, yy, func , facecolors=fcolors,rstride=rStepSize, cstride=cStepSize,linewidth=0, antialiased=aa, shade=False)
     else:
-        ax.plot_wireframe(xx, yy, POC, rstride=rStepSize, cstride=cStepSize,antialiased=aa)
+        ax.plot_wireframe(xx, yy, func, rstride=rStepSize, cstride=cStepSize,antialiased=aa)
 
-    plt.show()
-
-def displayInfoPOC(POC):
-    print("POC Max Peak: ", np.max(POC))
-
-def plotPOC(POC):
-    plt.figure()
-    plt.imshow(POC,cmap='gray')
     plt.show()
 
 def plotDFT(title, dft):
---
+++ main.py
@@ -0,0 +1,113 @@
+'''
+
+==================================================
+ Info:
+
+ Written by: Trym Nygård 
+ Last updated: March 2022
+
+ Required python script "func.py"
+'''
+
+''' 
+# PARAMETERS FOR HORSE IMAGE
+M = 60       # Width of window
+N = 60      # Height of window
+X_L = 50    # X center position of the left window
+X_R = 50    # X center position of the right window
+Y_L = 30    # Y center position of the left window
+Y_R = 30    # Y center position of the right window
+''' 
+
+# PARAMETERS FOR SEOUL IMAGE
+M = 50       # Width of window
+N = 50       # Height of window
+X_L = 150    # X center position of the left window
+X_R = 150    # X center position of the right window
+Y_L = 300    # Y center position of the left window
+Y_R = 300    # Y center position of the right window
+
+'''
+# PARAMETERS FOR SALMON IMAGE
+M = 30       # Width of window
+N = 30       # Height of window
+X_L = 1002    # X center position of the left window
+Y_L = 315   # Y center position of the left window
+X_R = 852    # X center position of the left window
+Y_R = 315    # Y center position of the left window
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2 as cv
+import func as fc
+
+# Load image
+#imgL = cv.imread('horse.png',0) 
+imgL = cv.imread('seoul.jpg',0) 
+#imgL = cv.imread('images/stereo_left/L1.jpg',0)
+#imgR = cv.imread('images/stereo_right/R1.jpg',0)
+
+# Create shifted image
+X_SHIFT = 7
+Y_SHIFT = 0
+translationMatrix = np.float32([[1, 0, X_SHIFT], [0, 1, Y_SHIFT]])
+imgR = cv.warpAffine(imgL, translationMatrix, (imgL.shape[1],imgL.shape[0]))
+
+# Resize
+SCALE_PERCENT = 10 # percent of original size
+imgL, imgR = fc.stereoImgResize(imgL,imgR,SCALE_PERCENT,cv.INTER_AREA)
+#print("Resized Image size: ", imgL.shape)
+
+print("*********************X-SHIFT = ",X_SHIFT/10,"]*********************")
+
+#Create window of features
+winL = fc.stereoImgCrop(imgL, X_L, Y_L, M, N)
+winR = fc.stereoImgCrop(imgR, X_R, Y_R, M, N)
+
+#Apply window function
+winL = fc.windowFunc("blackman",winL) #funcType: blackman, hanning, 
+winR = fc.windowFunc("blackman",winR) #funcType: blackman, hanning
+
+#winL, winR, newX = fc.blockMatching(imgL,imgR, X_L, Y_L, M, N)      
+
+# Discrete Fourier Transform of both images 
+dftL = np.fft.fft2(winL)
+dftR = np.fft.fft2(winR)
+
+# Compute magnitude plot
+dftShiftL = np.fft.fftshift(dftL)
+dftShiftR = np.fft.fftshift(dftR)
+magSpecL = 20*np.log(np.abs(dftShiftL))
+magSpecR = 20*np.log(np.abs(dftShiftR))
+
+''' 
+f, axarr = plt.subplots(2,2)
+axarr[0,0].imshow((winL),cmap='gray')
+axarr[0,1].imshow(winR,cmap='gray')
+axarr[1,0].imshow(magSpecL,cmap='gray')
+axarr[1,1].imshow(magSpecR,cmap='gray')
+plt.show()
+'''
+
+#**IMAGE REGISTRATION IN THE SPATIAL DOMAIN USING POC (INTEGER PRECISION)**
+
+POC = fc.computePOC(dftL,dftR)
+#fc.plotSurface(POC, wireframe=True)
+
+intShiftX,intShiftY =  fc.computePOCshift(POC)
+print("intShiftX: ",intShiftX, "                  [PC WITH DFT]")
+
+#*****************************PHASE PLOT*******************************
+#fc.plotPhaseDifference(dftL,dftR)
+
+#******IMAGE REGISTRATION IN THE FOURIER DOMAIN [SUB-PIXEL PRECISION]******
+subShiftX,subShiftY = fc.computeSubSpaceID(dftL,dftR,radius=0.4, magThres=0)
+
+print("subShiftX: ",round(subShiftX, 3), "                [SUB-PIXEL (Subspace identification)]")
+
+subShiftX2 = fc.computeGradCorr(winL,winR)
+print("subShiftX: ",round(subShiftX2,3), "                [SUB-PIXEL (Gradient Correlation)]")
+
+
+
